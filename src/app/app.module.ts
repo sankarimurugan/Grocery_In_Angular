b5a4d7022f777999fb8fed523a8753cc3a2e9d7b
M.Sankari <120647441+sankariparvath@users.noreply.github.com>
2023-04-21 06:09:47 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './components/home/home.component';
+import { ShopComponent } from './components/shop/shop.component';
+import { AboutComponent } from './components/about/about.component';
+import { ReviewComponent } from './components/review/review.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomComponent } from './components/hom/hom.component';
+ 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ShopComponent,
+    AboutComponent,
+    ReviewComponent,
+    BlogComponent,
+    ContactComponent,
+    NavComponent,
+    FooterComponent,
+    HomComponent
   ],
   imports: [
     BrowserModule,
